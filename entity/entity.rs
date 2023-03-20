894c42cb7dc8a7176fee0b18a68df54f68f8c6a9
mysll <mysll@sina.com>
2023-03-20 14:07:45 +0800

    update entity

--- entity/entity.rs
+++ entity/entity.rs
@@ -14,3 +14,71 @@ pub trait Object {
     fn get_attr_by_index<'a>(&'a self, index: u32) -> Option<&'a dyn Any>;
     fn set_attr_by_index(&mut self, index: u32, val: &dyn Any) -> bool;
 }
+
+#[allow(dead_code)]
+#[derive(Default)]
+pub struct EntityInfo {
+    pub attrs: Vec<&'static str>,
+    pub index: std::collections::HashMap<&'static str, u32>,
+    pub saves_index: Vec<u32>,
+    pub reps_index: Vec<u32>,
+    pub saves: Vec<&'static str>,
+    pub reps: Vec<&'static str>,
+    pub dirty: bool,
+    pub modify_attrs: Vec<u32>,
+}
+
+impl EntityInfo {
+    pub fn init(
+        &mut self,
+        attrs: Vec<&'static str>,
+        saves: Vec<&'static str>,
+        reps: Vec<&'static str>,
+    ) {
+        self.attrs = attrs;
+        self.saves = saves;
+        self.reps = reps;
+        self.attrs.iter().enumerate().for_each(|(i, attr)| {
+            self.index.insert(attr, i as u32);
+        });
+        self.saves
+            .iter()
+            .enumerate()
+            .for_each(|(_, attr)| self.saves_index.push(self.index[attr]));
+        self.reps
+            .iter()
+            .enumerate()
+            .for_each(|(_, attr)| self.reps_index.push(self.index[attr]));
+    }
+
+    pub fn attr_count(&self) -> u32 {
+        self.attrs.len() as u32
+    }
+    pub fn get_attr_index(&self, attr: &str) -> Option<u32> {
+        match self.index.get(attr) {
+            Some(&i) => Some(i),
+            None => None,
+        }
+    }
+    pub fn get_attr_name<'a>(&'a self, index: u32) -> Option<&'a str> {
+        match self.attrs.get(index as usize) {
+            Some(&attr) => Some(attr),
+            None => None,
+        }
+    }
+    pub fn dirty(&self) -> bool {
+        self.dirty
+    }
+    pub fn clear_dirty(&mut self) {
+        self.dirty = false;
+    }
+    pub fn modify(&self) -> bool {
+        self.modify_attrs.len() > 0
+    }
+    pub fn get_modify<'a>(&'a self) -> &'a Vec<u32> {
+        &self.modify_attrs
+    }
+    pub fn clear_modify(&mut self) {
+        self.modify_attrs.clear();
+    }
+}
--- ops/lib.rs
+++ ops/lib.rs
@@ -17,14 +17,7 @@ pub fn def_entity(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = parse_macro_input!(args as parse::Nothing);
     if let syn::Fields::Named(ref mut fields) = item_struct.fields {
         let add_attr = vec![
-            quote! {__attrs: Vec<&'static str>},
-            quote! {__saves_index: Vec<u32>},
-            quote! {__reps_index: Vec<u32>},
-            quote! {__saves: Vec<&'static str>},
-            quote! {__reps: Vec<&'static str>},
-            quote! {__index: std::collections::HashMap<&'static str,u32>},
-            quote! {__dirty: bool},
-            quote! {__modify_attrs: Vec<u32>},
+            quote! {pub __internal: re_entity::entity::EntityInfo},
         ];
         for att in add_attr {
             fields
@@ -96,7 +89,11 @@ pub fn entity_builder(input: TokenStream) -> TokenStream {
                         &self.#ident_field
                     }
                     pub fn #set(&mut self, val:#ty) {
+                        if self.#ident_field == val {
+                            return;
+                        }
                         self.#ident_field = val
+                        
                     }
                     pub fn #get_enum(&self) -> #ident_enum{
                         #ident_enum::#attr_enum
@@ -142,25 +139,26 @@ pub fn entity_builder(input: TokenStream) -> TokenStream {
             #(#attrs_up),*
         }
 
-
         impl #ident {
             pub fn new() -> Self {
                 let mut d = Self::default();
-                d.__attrs = vec![ #(stringify!(#attrs)),* ];
-                d.__index = std::collections::HashMap::new();
-                d.__attrs.iter()
-                .enumerate()
-                .for_each(|(i, attr)| {d.__index.insert(attr, i as u32);});
-
-                d.__saves_index = Vec::new();
-                d.__saves = vec![ #(stringify!(#save_attrs)),* ];
-                d.__saves.iter().enumerate().for_each(|(_, attr)| {d.__saves_index.push(d.__index[attr])});
-
-                d.__reps_index = Vec::new();
-                d.__reps = vec![ #(stringify!(#rep_attrs)),* ];
-                d.__reps.iter().enumerate().for_each(|(_, attr)| {d.__reps_index.push(d.__index[attr])});
-
+                let attrs = vec![ #(stringify!(#attrs)),* ];
+                let saves = vec![ #(stringify!(#save_attrs)),* ];
+                let reps = vec![ #(stringify!(#rep_attrs)),* ];
+                d.__internal.init(attrs, saves, reps);
                 d
+            }
+            pub fn dirty(&self) -> bool {
+                self.__internal.dirty()
+            }
+            pub fn clear_dirty(&mut self) {
+                self.__internal.clear_dirty()
+            }
+            pub fn modify(&self) -> bool {
+                self.__internal.modify()
+            }
+            pub fn clear_modify(&mut self) {
+                self.__internal.clear_modify()
             }
             pub fn set_attr(&mut self, att: #ident_enum, v :&dyn std::any::Any) -> bool {
                 match att {
@@ -178,46 +176,40 @@ pub fn entity_builder(input: TokenStream) -> TokenStream {
         use re_entity::entity::Object;
         impl Object for #ident {
             fn get_attrs<'a>(&'a self) -> &'a Vec<&str> {
-                &self.__attrs
+                &self.__internal.attrs
             }
             fn save_attrs<'a>(&'a self) -> &'a Vec<&str>{
-                &self.__saves
+                &self.__internal.saves
             }
             fn rep_attrs<'a>(&'a self) -> &'a Vec<&str>{
-                &self.__reps
+                &self.__internal.reps
             }
             fn save_attrs_index(&self) -> &Vec<u32> {
-                &self.__saves_index
+                &self.__internal.saves_index
             }
             fn rep_attrs_index(&self) -> &Vec<u32> {
-                &self.__reps_index
+                &self.__internal.reps_index
             }
             fn get_attr_count(&self) -> u32 {
-                self.__attrs.len() as u32
+                self.__internal.attr_count()
             }
             fn get_attr_by_name<'a>(&'a self, attr: &str) -> Option<&'a dyn std::any::Any> {
-                match self.__index.get(attr) {
-                    Some(&i) => self.get_attr_by_index(i),
+                match self.__internal.get_attr_index(attr) {
+                    Some(i) => self.get_attr_by_index(i),
                     None=> None
                 }
             }
             fn set_attr_by_name(&mut self, attr: &str, val: &dyn std::any::Any) -> bool {
-                match self.__index.get(attr) {
-                    Some(&i) => self.set_attr_by_index(i, val),
+                match self.__internal.get_attr_index(attr) {
+                    Some(i) => self.set_attr_by_index(i, val),
                     None=> false
                 }
             }
             fn get_attr_name<'a>(&'a self, index: u32) -> Option<&'a str> {
-                match self.__attrs.get(index as usize) {
-                    Some(attr) => Some(attr),
-                    None=>None,
-                }
+                self.__internal.get_attr_name(index)
             }
             fn get_attr_index(&self, attr: &str) -> Option<u32> {
-                match self.__index.get(attr) {
-                    Some(&i) => Some(i),
-                    None=> None
-                }
+                self.__internal.get_attr_index(attr)
             }
             fn get_attr_by_index<'a>(&'a self, index: u32) -> Option<&'a dyn std::any::Any> {
                 if let Some(att) = #ident_enum::get_attr_enum_by_index(index) {
